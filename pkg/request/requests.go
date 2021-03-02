f40bf7e8c2c8a6b1ccb0e3675de842d7d09c8fb8
csothen <cesarjcpinheiro@gmail.com>
2021-03-02 15:28:35 +0000

    Remove user agent header

--- pkg/request/requests.go
+++ pkg/request/requests.go
@@ -7,7 +7,6 @@ func Get(url string) (*http.Response, error) {
 	client := &http.Client{}
 
 	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
 
 	res, err := client.Do(req)
 	if err != nil {
