78e52c76d5fdd1b2ba275c1f8e102b49e281ffce
csothen <cesarjcpinheiro@gmail.com>
2021-03-03 17:00:06 +0000

    Add more time to Read and Write Timeouts

--- cmd/htmlparser/main.go
+++ cmd/htmlparser/main.go
@@ -42,8 +42,8 @@ func configServer(router *mux.Router) *http.Server {
 		Addr:         ":9090",
 		Handler:      router,
 		ErrorLog:     logger,
-		ReadTimeout:  30 * time.Second,
-		WriteTimeout: 45 * time.Second,
+		ReadTimeout:  60 * time.Second,
+		WriteTimeout: 60 * time.Second,
 		IdleTimeout:  120 * time.Second,
 	}
 
